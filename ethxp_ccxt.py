82955cbd6389318b126145243424d862dc32908d
Rafael <rafaelvleite@icloud.com>
2022-12-06 14:59:29 -0300

    book

--- ethxp_ccxt.py
+++ ethxp_ccxt.py
@@ -6,10 +6,9 @@ import ccxt
 print(ccxt.exchanges) # print a list of all available exchange classes
 
 deribit = ccxt.deribit()
-
 deribit_markets = deribit.load_markets()
+
 
 print(deribit.id, deribit.load_markets())
 print(deribit.fetch_ticker('BTC-IBUT-29SEP23-5000_22000_80000'))
-
-
+print(deribit.fetch_order_book(deribit.symbols[0]))
